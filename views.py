27c514b24ff05a3ab3bd0da52e16b17937deb42b
andre_ochoa <andresalvadorochoa8amtz@gmail.com>
2022-05-07 10:14:21 -0500

    init comit

---
+++ views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from .models import Producto
+
+# Create your views here.
+
+def tienda(request):
+
+    productos=Producto.objects.all()
+
+    return render(request, "tienda/tienda.html", {"productos":productos})
